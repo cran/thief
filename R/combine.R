10389fe68208ef89eaec678a6dc99a22b042a965
Rob Hyndman <Rob.Hyndman@monash.edu>
2016-09-07 08:49:16 +0000

    version 0.2

---
+++ R/combine.R
@@ -0,0 +1,190 @@
+#' Reconcile temporal hierarchical forecasts
+#'
+#' Takes forecasts of time series at all levels of temporal aggregation
+#' and combines them using the temporal hierarchical approach of Athanasopoulos et al (2016).
+#'
+#' @param forecasts List of forecasts. Each element must be a time series of forecasts,
+#' or a forecast object.
+#' The number of forecasts should be equal to k times the seasonal period for each series,
+#' where k is the same across all series.
+#' @param comb Combination method of temporal hierarchies, taking one of the following values:
+#' \describe{
+#'   \item{"struc"}{Structural scaling - weights from temporal hierarchy}
+#'   \item{"mse"}{Variance scaling - weights from in-sample MSE}
+#'   \item{"ols"}{Unscaled OLS combination weights}
+#'   \item{"bu"}{Bottom-up combination -- i.e., all aggregate forecasts are ignored.}
+#'   \item{"shr"}{GLS using a shrinkage (to block diagonal) estimate of residuals}
+#'   \item{"sam"}{GLS using sample covariance matrix of residuals}
+#' }
+#' @param mse A vector of one-step MSE values corresponding to each of the forecast series.
+#' @param residuals List of residuals corresponding to each of the forecast models.
+#' Each element must be a time series of residuals. If \code{forecast} contains a list of
+#' forecast objects, then the residuals will be extracted automatically and this argument
+#' is not needed. However, it will be used if not \code{NULL}.
+#' @param returnall If \code{TRUE}, a list of time series corresponding to the first argument
+#' is returned, but now reconciled. Otherwise, only the most disaggregated series is returned.
+#'
+#' @return
+#'   List of reconciled forecasts in the same format as \code{forecast}.
+#' If \code{returnall==FALSE}, only the most disaggregated series is returned.
+#' @seealso \code{\link{thief}}, \code{\link{tsaggregates}}
+#'
+#' @examples
+#' # Construct aggregates
+#' aggts <- tsaggregates(USAccDeaths)
+#'
+#' # Compute forecasts
+#' fc <- list()
+#' for(i in seq_along(aggts))
+#'   fc[[i]] <- forecast(auto.arima(aggts[[i]]), h=2*frequency(aggts[[i]]))
+#'
+#' # Reconcile forecasts
+#' reconciled <- reconcilethief(fc)
+#'
+#' # Plot forecasts before and after reconcilation
+#' par(mfrow=c(2,3))
+#' for(i in seq_along(fc))
+#' {
+#'   plot(reconciled[[i]], main=names(aggts)[i])
+#'   lines(fc[[i]]$mean, col='red')
+#' }
+#'
+#' @export
+#' @author Rob J Hyndman
+
+reconcilethief <- function(forecasts,
+               comb=c("struc","mse","ols","bu","shr","sam"),
+               mse=NULL, residuals=NULL, returnall=TRUE)
+{
+  comb <- match.arg(comb)
+
+  # If forecasts is a list of forecast objects, then
+  # extract list of forecast time series and list of residual time series
+  if(is.element("forecast",class(forecasts[[1]])))
+  {
+    returnclass <- "forecast"
+    origf <- forecasts
+    # Grab residuals
+    if(is.null(residuals))
+    {
+      residuals <- list()
+      for(i in seq_along(forecasts))
+        residuals[[i]] <- residuals(forecasts[[i]])
+      # Discard partial years at start of residual series
+      for(i in seq_along(residuals))
+      {
+        tspy <- tsp(residuals[[i]])
+        m <- tspy[3]
+        fullyears <- trunc(length(residuals[[i]])/m)
+        residuals[[i]] <- ts(utils::tail(residuals[[i]], fullyears*m), frequency=m, end=tspy[2])
+      }
+    }
+    # Grab forecasts
+    for(i in seq_along(forecasts))
+      forecasts[[i]] <- forecasts[[i]]$mean
+  }
+  else
+    returnclass <- "ts"
+
+  # Find seasonal periods
+  freq <- unlist(lapply(forecasts,frequency))
+  if(min(freq) != 1L)
+    stop("Minimum seasonal period should be 1")
+  m <- max(freq)
+  # Put in order
+  k <- rev(order(freq))
+  forecasts <- forecasts[k]
+  freq <- freq[k]
+
+  # Check series each have same equivalent lengths.
+  lengths <- unlist(lapply(forecasts, length))
+  if(!is.constant(lengths/freq))
+    stop("Forecast series must have the same equivalent lengths")
+
+  # Bottom up
+  if(comb == "bu")
+    bts <- forecasts[[1]]
+  # Some combination
+  else
+  {
+    # Set up group matrix for hts
+    nsum <- rep(freq,m/freq)
+    unsum <- unique(nsum)
+    grps <- matrix(0, nrow=length(unsum)-1, ncol=m)
+    for(i in 1:(length(unsum)-1))
+    {
+      mi <- m/unsum[i]
+      grps[i,] <- rep(1:mi, rep(unsum[i],mi))
+    }
+
+    # Set up matrix of forecasts in right structure
+    nc <- length(forecasts[[1]])/m
+    fmat <- matrix(0, nrow=0, ncol=nc)
+    for(i in rev(seq_along(forecasts)))
+      fmat <- rbind(fmat, matrix(forecasts[[i]], ncol=nc))
+
+    # OLS or WLS reconciliation
+    if(is.element(comb, c("struc","ols","mse")))
+    {
+      if(comb=="struc")
+        weights <- 1/nsum
+      else if(comb=="ols")
+        weights <- NULL
+      else if(comb=="mse")
+        weights <- 1/rep(rev(mse), rev(unsum))
+      bts <- hts::combinef(t(fmat), groups=grps, weights=weights, keep='bottom')
+    }
+    else   # GLS reconciliation
+    {
+      # Set up matrix of residuals in right structure
+      if(is.null(residuals))
+        stop("GLS needs residuals")
+      nc <- length(residuals[[1]])/m
+      rmat <- matrix(0, nrow=0, ncol=nc)
+      for(i in rev(seq_along(forecasts)))
+        rmat <- rbind(rmat, matrix(residuals[[i]], ncol=nc))
+      bts <- hts::MinT(t(fmat), groups=grps, residual=t(rmat),  covariance=comb, keep='bottom')
+    }
+    # Turn resulting reconciled forecasts back into a ts object
+    bts <- ts(c(t(bts)))
+    tsp(bts) <- tsp(forecasts[[1]])
+  }
+
+  # Now figure out what to return
+  if(returnclass=="ts") # Just return time series
+  {
+    if(!returnall)
+      return(bts)
+    else
+      return(tsaggregates(bts))
+  }
+  else #return forecast objects
+  {
+    if(!returnall)
+    {
+      adj <- bts - origf[[1]]$mean
+      origf[[1]]$mean <- bts
+      if(!is.null(origf[[1]]$lower))
+      {
+        origf[[1]]$lower <- sweep(origf[[1]]$lower,1,adj,"+")
+        origf[[1]]$upper <- sweep(origf[[1]]$upper,1,adj,"+")
+      }
+      return(origf[[1]])
+    }
+    else
+    {
+      allts <- tsaggregates(bts)
+      for(i in seq_along(origf))
+      {
+        adj <- allts[[i]] - origf[[i]]$mean
+        origf[[i]]$mean <- allts[[i]]
+        if(!is.null(origf[[i]]$lower))
+        {
+          origf[[i]]$lower <- sweep(origf[[i]]$lower,1,adj,"+")
+          origf[[i]]$upper <- sweep(origf[[i]]$upper,1,adj,"+")
+        }
+      }
+      return(origf)
+    }
+  }
+}
---
+++ R/thief.R
@@ -0,0 +1,234 @@
+#' Temporal hierarchical forecasting
+#'
+#' Takes a time series as input and produces forecasts using
+#' the temporal hierarchical approach of Athanasopoulos et al (2016).
+#'
+#' @param y        Time series input
+#' @param m        Seasonal period
+#' @param h        Forecast horizon
+#' @param comb     Combination method of temporal hierarchies, taking one of the following values:
+#' \describe{
+#'   \item{"struc"}{Structural scaling - weights from temporal hierarchy}
+#'   \item{"mse"}{Variance scaling - weights from in-sample MSE}
+#'   \item{"ols"}{Unscaled OLS combination weights}
+#'   \item{"bu"}{Bottom-up combination -- i.e., all aggregate forecasts are ignored.}
+#'   \item{"shr"}{GLS using a shrinkage (to block diagonal) estimate of residuals}
+#'   \item{"sam"}{GLS using sample covariance matrix of residuals}
+#' }
+#' @param usemodel    Model used for forecasting each aggregation level:
+#' \describe{
+#'   \item{"ets"}{exponential smoothing, using the \code{\link[forecast]{ets}} function.}
+#'   \item{"arima"}{arima, using the \code{\link[forecast]{auto.arima}} function.}
+#'   \item{"theta"}{theta method, using the \code{\link[forecTheta]{stm}} function.}
+#'   \item{"naive"}{random walk forecasts}
+#'   \item{"snaive"}{seasonal naive forecasts, based on the last year of observed data.}
+#' }
+#' @param forecastfunction User-defined function to be used instead of \code{usemodel}. The
+#' function must take a time series as the first argument, and the forecast horizon 
+#' as the second argument. It must return an object of class \code{forecast}.
+#' @param ...   Arguments to be passed to the time series modelling function 
+#' (such as \code{ets} or \code{auto.arima}), or to \code{forecastfunction}.
+#' 
+#' @details This function computes the temporal aggregates of \code{y} using 
+#' \code{\link{tsaggregates}}, then calculates all forecasts using the model function 
+#' specified by \code{usemodel} or \code{forecastfunction}, and finally reconciles the 
+#' forecasts using \code{\link{reconcilethief}}. The reconciled forecasts of \code{y} 
+#' are returned.
+#'
+#' @return
+#'   forecast object.
+#' 
+#' @seealso \code{\link{reconcilethief}}
+#' 
+#' @examples
+#' \dontrun{
+#' 
+#' # Select ARIMA models for all series using auto.arima()
+#' z <- thief(AEdemand[,12], usemodel='arima')
+#' plot(z)
+#' 
+#' # Use your own function
+#' ftbats <- function(y,h,...){forecast(tbats(y),h)}
+#' z <- thief(AEdemand[,12], forecastfunction=ftbats)
+#' plot(z)
+#' }
+#'
+#' @export
+#' @import forecast
+#' @author Rob J Hyndman and Nikolaos Kourentzes
+
+thief <- function(y, m=frequency(y), h=m*2,
+               comb=c("struc","mse","ols","bu","shr","sam"),
+               usemodel=c("ets","arima","theta","naive","snaive"), 
+               forecastfunction=NULL, ...)
+{
+  comb <- match.arg(comb)
+  if(is.null(forecastfunction))
+    usemodel <- match.arg(usemodel)
+  else
+    usemodel <- deparse(substitute(forecastfunction))
+
+  # Check input is a univariate time series
+  if(!is.element("ts",class(y)))
+    stop("y must be a time series object")
+  if(NCOL(y) > 1L)
+    stop("y must be a univariate time series")
+
+  # Make sure the time series is seasonal
+  if(m <= 1L)
+    stop("Seasonal period (m) must be greater than 1")
+  if(length(y) < 2*m)
+    stop("I need at least 2 periods of data")
+
+  # Compute aggregate
+  aggy <- tsaggregates(y)
+
+  # Compute forecasts
+  frc <- th.forecast(aggy, h=h, usemodel=usemodel, 
+    forecastfunction=forecastfunction, ...)
+
+  # Reconcile forecasts and fitted values
+  bts <- reconcilethief(frc$forecast, comb, frc$mse, frc$residuals, returnall=FALSE)
+  fits <- reconcilethief(frc$fitted, comb, frc$mse, frc$residuals, returnall=FALSE)
+
+  # Construct forecast object to return
+  out <- structure(list(x=y, mean=bts, fitted=fits, residuals=y-fits),
+    class='forecast')
+  out$method <- paste("THieF-",toupper(usemodel),sep="")
+  return(out)
+}
+
+#-------------------------------------------------
+th.forecast <- function(aggy, h=NULL, usemodel, forecastfunction, ...)
+{
+# Produce forecasts for multiple temporal aggregation levels with predefined models
+#
+# Inputs:
+#   aggy        List of time series to be forecast
+#   h           Forecast horizon. Default is m*2
+#   usemodel    Model used for forecasting each aggregation level
+#
+# Outputs:
+#   frc         Uncombined temporal hierarchy forecasts
+#   fitted      Uncombined temporal hierarchy fitted values
+#   residual    Uncombined temporal hierarchy residuals
+#   mse         Mean squared errors for each temporal aggregation level
+#   mseh        Mean squared errors per horizon for each temporal aggregation level
+#
+# Example:
+#   aggy <- tsaggregates(USAccDeaths)
+#   th.forecast(aggy,h=20)
+#
+# Nikolaos Kourentzes and Rob J Hyndman
+
+  # Calculate forecast horizons
+  n.k <- length(aggy)
+  freq <- unlist(lapply(aggy, frequency))
+  m <- max(freq)
+  AL <- m/freq
+  H <- m/AL*ceiling(h/m) 
+
+  # Initialise
+  frc <- fitted <- resid <- mseh <- vector("list",n.k)
+  mse <- vector("numeric",n.k)
+
+  # Model estimation and forecasts
+  for (k in 1:n.k)
+  {
+    temp <- th.forecast.loop(aggy[[k]], H[[k]], usemodel, forecastfunction, ...)
+    frc[[k]] <- temp$frc
+    fitted[[k]] <- temp$fitted
+    resid[[k]] <- temp$resid
+    mse[k] <- temp$mse
+    mseh[[k]] <- temp$mseh
+  }
+
+  # Return forecasts
+  #names(frc) <- names(fitted) <- names(resid) <- names(mseh) <- names(mse) <- names(aggy)
+
+  return(list(forecast=frc,fitted=fitted,residuals=resid,mse=mse,mseh=mseh))
+}
+
+#-------------------------------------------------
+th.forecast.loop <- function(y, h, usemodel, forecastfunction, ...){
+# Loop for model forecasts
+# Produce forecasts and other information for univariate series y
+# Forecast horizon h
+# Return list of forecasts, fitted values, residuals, mse, etc.
+
+  m <- frequency(y)
+
+  if(!is.null(forecastfunction))
+    fc <- forecastfunction(y, h, level=80,...)
+  else if (usemodel=="ets")
+  {
+    fit <- try(forecast::ets(y, ...), silent=TRUE)
+    # Do something simpler if ets model doesn't work
+    # This is only necessary for versions of the package before 22 Aug 2016 (7.2beta)
+    if(is.element("try-error",class(fit)))
+    {
+      # Use HW if there is enough data and the data is seasonal
+      if(frequency(y) > 1L & length(y) >= 2*frequency(y))
+        fit <- fc <- forecast::hw(y, h=h, level=80, 
+                            initial='simple', alpha=0.2, beta=0.1, gamma=0.01)
+      else # Otherwise just use Holt's
+        fit <- fc <- forecast::holt(y, h=h, level=80, 
+                               initial='simple', alpha=0.2, beta=0.1)
+    }
+    else
+      fc <- forecast::forecast(fit, h=h, level=80)
+  }
+  else if(usemodel=="arima")
+  {
+    fit <- forecast::auto.arima(y, ...)
+    fc <- forecast::forecast(fit, h=h, level=80)
+  } 
+  else if (usemodel == "theta")
+  {
+    if(length(y) < 5)
+      fc <- forecast::thetaf(y, h=h, level=80)
+    else
+    {
+      fc <- forecTheta::stm(y, h=h, level=80, ...)
+      fc$x <- fc$y
+      fc$lower <- ts(matrix(fc$lower, ncol=1))
+      fc$upper <- ts(matrix(fc$upper, ncol=1))
+      tsp(fc$lower) <- tsp(fc$upper) <- tsp(fc$mean)
+      class(fc) <- "forecast"
+    }
+  }
+  else 
+  {
+    # Seasonal naive forecast
+    if(usemodel=='snaive')
+      fc <- forecast::snaive(y, h=h, level=80)
+    # Naive forecast
+    else
+      fc <- forecast::naive(y, h=h, level=80)
+  }
+
+  # Calculate fitted residuals and MSE
+  fitted <- fitted(fc)
+  if(is.null(fitted) & !is.null(fc$residuals))
+    fitted <- y - fc$residuals
+  if(!is.null(fitted))
+    resid <- y - fitted
+  else
+    resid <- NULL
+  mse <- mean(resid^2, na.rm=TRUE)
+
+  # Only keep full years of fitted values and residuals
+  # Remove partial year from start
+  tspy <- tsp(y)
+  fullyears <- trunc(length(y) / m)
+  fitted <- ts(utils::tail(fitted, fullyears*m), frequency=m, end=tspy[2])
+  resid <- ts(utils::tail(resid, fullyears*m), frequency=m, end=tspy[2])
+
+  # Compute MSE and MSEH
+  if(!is.null(fc$lower))
+    mseh <- ((fc$mean - fc$lower[,1])/stats::qnorm(0.9))^2
+  else
+    mseh <- NULL
+
+  return(list(frc=fc$mean,mse=mse,mseh=mseh,fitted=fitted,resid=resid))
+}
